43a0300c89fd4ebf041c2b809c5d23af0499a6c6
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:23:52 +0500

    Create 9.ts

---
+++ Question no.9/9.ts
@@ -0,0 +1,12 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                                                                                        Question 9 of 45 Questions
+
+// QUESTION NO:9
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+// Zargham Abbas , 6-April-2024
+// This program prints my name.
+let myname:string = "Zargham";
+console.log(`My name is ${myname}`);
