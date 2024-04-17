371b617aff250839e6a4993415d506a9d9fc58a4
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:06:34 +0500

    Create 1.ts

---
+++ Question no.1/1.ts
@@ -0,0 +1,11 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                         Question 1 of 45 Questions
+
+
+// QUESTION NO:1
+// Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+let myName1: string = "Zargham";
+console.log(`"Hello ${myName1}, would you like to learn some TypeScript today?”`);
