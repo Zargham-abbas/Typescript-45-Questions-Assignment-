207781206fa8acf7f579eb63f956ef524c2fe19b
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:08:56 +0500

    Create 2.ts

---
+++ Question no.2/2.ts
@@ -0,0 +1,13 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                              Question 2 of 45 Questions
+
+
+// QUESTION NO:2
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let myName2: string = "Zargham";
+console.log(myName2.toUpperCase());
+console.log(myName1.toLowerCase());
+console.log(myName2.charAt(0).toUpperCase() + myName2.slice(1).toLowerCase());
