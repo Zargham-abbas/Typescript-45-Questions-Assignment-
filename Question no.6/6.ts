63587d8b11b2b89086dcb4f6757b8124ec409ecd
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:18:21 +0500

    Create 6.ts

---
+++ Question no.6/6.ts
@@ -0,0 +1,13 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                               Question 6 of 45 Questions
+
+
+// QUESTION NO:6
+//  Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+console.log(5+3);
+console.log(12-4);
+console.log(2*4);
+console.log(16/2);
