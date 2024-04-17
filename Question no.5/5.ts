4d2192731e2d2b301118bc506ea3071c0d75ad0e
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:17:09 +0500

    Create 5.ts

---
+++ Question no.5/5.ts
@@ -0,0 +1,12 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                                                                 Question 5 of 45 Questions
+
+
+// QUESTION NO:5
+//  Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+let myName3:string = "\t\n Zargham \t\n";
+console.log(myName3)
+console.log(myName3.trim());
