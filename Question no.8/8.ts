72bc0efbbed891dbe2731b875677b8ae31711f5d
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:20:55 +0500

    Create 8.ts

---
+++ Question no.8/8.ts
@@ -0,0 +1,11 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                 Question 8 of 45 Questions
+
+
+// QUESTION NO:8
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+let favoriteNumber:number = 8;
+console.log(`My favourite number is ${favoriteNumber}.`);
