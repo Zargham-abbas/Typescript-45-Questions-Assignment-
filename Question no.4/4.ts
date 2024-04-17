499d72b73c503ba1b7481281169068ff63f14606
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:14:29 +0500

    Create 4.ts

---
+++ Question no.4/4.ts
@@ -0,0 +1,12 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//                                                                                  Question 4 of 45 Questions
+
+
+// QUESTION NO:4
+// Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_Person:string = "Albert Einstein";
+let message:string = `${famous_Person},once said, “A person who never made a mistake never tried anything new.”`
+console.log(message);
