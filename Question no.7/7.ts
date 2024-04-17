44d8fa0f664f63f85a1e5a2438ebb90fe223b225
Zargham Abbas <zarghamabbas859@gmail.com>
2024-04-17 17:19:43 +0500

    Create 7.ts

---
+++ Question no.7/7.ts
@@ -0,0 +1,14 @@
+// Zargham Abbas
+// zarghamabbas859@gmail.com
+// 0334-3591835
+
+//           Question 7 of 45 Questions
+
+
+// QUESTION NO:7
+// You should create four lines that look like this:
+// console.log(5 + 3)
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
